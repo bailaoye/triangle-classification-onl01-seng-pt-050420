d6bf061b25731bafbc9ad95f14508f4f44f222c2
Jeff Bai <j.bai@wustl.edu>
2020-06-11 07:45:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,12 @@ class Triangle
   attr_accessor :A, :B, :C
   @sides = []
 
+  def initialize(A, B, C)
+    @A = A
+    @B = B
+    @C = C
+  end
+
   end
   class TriangleError < StandardError
   end
